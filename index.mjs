c9660f03e784fcf5100881c0976b589ce68894c2
Sipu-982 <sipurana38@gmail.com>
2026-02-06 15:20:14 +0530

    .yml file updated

--- index.mjs
+++ index.mjs
@@ -1,27 +1,11 @@
 import express from "express";
-import cors from "cors";
 import serverless from "serverless-http";
 
-import DocRoutes from "./routes/career.routes.js";
-import adminRoutes from "./routes/admin.routes.js";
-
 const app = express();
-
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
-
-/* Routes */
-app.use("/api/career", DocRoutes);
-app.use("/api/admin", adminRoutes);
-
 app.get("/health", (req, res) => {
-  res.status(200).json({ status: "OK" });
+  res.json({ status: "OK" });
 });
 
 export const handler = serverless(app);
